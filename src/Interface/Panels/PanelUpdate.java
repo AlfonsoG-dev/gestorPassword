40b1e35f4a073b0b3f4679902cdf4025fb707096
tuNovio <alfgomachi@gmail.com>
2023-12-19 22:43:39 -0500

    feat: update feature and ok, cancel action handler

--- src/Interface/Panels/PanelUpdate.java
+++ src/Interface/Panels/PanelUpdate.java
@@ -12,8 +12,10 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import Conexion.Query.QueryDAO;
 import Config.DbConfig;
 import Mundo.Cuentas.Cuenta;
+import Mundo.Cuentas.CuentaBuilder;
 
 public class PanelUpdate {
 
@@ -51,8 +53,33 @@ public class PanelUpdate {
         return pOptions;
     }
 
-    private void OkButtonHandler(JButton OKButton, DbConfig myConfig) {
-        String registro = "";
+    private void OkButtonHandler(JButton OKButton, DbConfig myConfig, Cuenta toUpdate) {
+        OKButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(txtNombre.getText() != null && txtNombre.getText() != toUpdate.getNombre()) {
+                    toUpdate.setNombre(txtNombre.getText());
+                }
+                if(txtEmail.getText() != null && txtEmail.getText() != toUpdate.getEmail()) {
+                    toUpdate.setEmail(txtEmail.getText());
+                }
+                if(txtPassword.getText() != null && txtPassword.getText() != toUpdate.getPassword()) {
+                    toUpdate.setPassword(txtPassword.getText());
+                }
+                toUpdate.setUpdate_at();
+                try {
+                    QueryDAO<Cuenta> cuentaDAO = new QueryDAO<Cuenta>("cuenta", myConfig);
+                    String condition = "id_pk: " + toUpdate.getId_pk();
+                    if(JOptionPane.showConfirmDialog(myFrame, "Do you want to update?", "Update operation",
+                            JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION) {
+                        cuentaDAO.UpdateRegister(toUpdate, condition, "and", new CuentaBuilder());
+                        new PanelPrincipal(myConfig);
+                        myFrame.setVisible(false);
+                    }
+                } catch(Exception er) {
+                    System.err.println(er);
+                }
+            }
+        });
     }
     private void CancelButtonHandler(JButton cancelButton, DbConfig myConfig) {
         cancelButton.addActionListener(new ActionListener() {
@@ -65,12 +92,14 @@ public class PanelUpdate {
             }
         });
     }
-    private JPanel OperationsComponent(DbConfig myConfig) {
-        // TODO: add functiolality of ok and cancel button
+    private JPanel OperationsComponent(DbConfig myConfig, Cuenta toUpdate) {
         JPanel pOperations = new JPanel();
         pOperations.setLayout(new FlowLayout());
         JButton oKButton = new JButton("OK");
         pOperations.add(oKButton);
+        OkButtonHandler(oKButton, myConfig, toUpdate);
+
+
         JButton cancelButton = new JButton("Cancel");
         pOperations.add(cancelButton);
         CancelButtonHandler(cancelButton, myConfig);
@@ -87,7 +116,7 @@ public class PanelUpdate {
         pPrincipal.setLayout(new GridLayout(2, 1));
 
         pPrincipal.add(OptionsComponent(updateCuenta));
-        pPrincipal.add(OperationsComponent(myConfig));
+        pPrincipal.add(OperationsComponent(myConfig, updateCuenta));
 
         myFrame.add(headerLabel);
         myFrame.add(pPrincipal);
