0c43d2a8488f60a8816e89e6c03d4a5d22f600fd
tuNovio <alfgomachi@gmail.com>
2024-01-14 15:13:14 -0500

    refactor: user root for connection

--- src/gestorPassword.java
+++ src/gestorPassword.java
@@ -3,7 +3,7 @@ import Interface.Panels.PanelLogin;
 class gestorPassword {
     public static void main(String[] args) {
         try {
-            DbConfig mConfig = new DbConfig("contrasenias", "localhost", "3306", "test_user", "5x5W12");
+            DbConfig mConfig = new DbConfig("contrasenias", "localhost", "3306", "root", "5x5W12%$");
             new PanelLogin(mConfig);
         } catch(Exception e) {
             System.err.println(e);
