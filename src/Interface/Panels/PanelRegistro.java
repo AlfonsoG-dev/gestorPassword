8b99c53a53d208da4e6cff7fd292ed1c9554c258
tuNovio <alfgomachi@gmail.com>
2024-01-22 18:12:15 -0500

    refactor: add missing dependency

--- src/Interface/Panels/PanelRegistro.java
+++ src/Interface/Panels/PanelRegistro.java
@@ -6,6 +6,7 @@ import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
