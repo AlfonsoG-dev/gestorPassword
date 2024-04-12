7f03e68a9457b7f385e55050b9315fa357a2a43a
AlfonsoG-dev <alfgomachi@gmail.com>
2024-04-12 17:42:09 -0500

    refactor: change lib dependency to javaMysqlORM

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,4 +1,4 @@
-$compile = "javac -Xlint:all -d .\bin\ -cp '.\lib\java-mysql-eje\java-mysql-eje.jar' .\src\*.java .\src\Interface\Panels\*.java .\src\Interface\Utils\*.java "
+$compile = "javac -Xlint:all -d .\bin\ -cp '.\lib\javaMysqlORM\javaMysqlORM.jar' .\src\*.java .\src\Interface\Panels\*.java .\src\Interface\Utils\*.java "
 $createJar = "jar -cfm gestorPassword.jar Manifesto.txt -C .\bin\ ."
 $javaCommand = "java -jar gestorPassword.jar"
 $runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
