eacd8f67ea6cc5c8fd6439ea191aa3ec0fac7ab1
AlfonsoG-dev <alfgomachi@gmail.com>
2026-02-24 18:49:54 -0500

    build: change build script

---
+++ build.ps1
@@ -0,0 +1,7 @@
+$srcClasses = "src\application\*.java src\application\interfaces\panels\*.java src\application\interfaces\utils\*.java src\application\models\cuenta\*.java src\application\models\user\*.java "
+$libFiles = ".\lib\javaORM_2.0\javaORM_2.0.jar;"
+$compile = "javac --release 23 -Xlint:all -Xdiags:verbose -d .\bin\ -cp '$libFiles' $srcClasses"
+$createJar = "jar -cfm gestorPassword.jar Manifesto.txt -C .\bin\ ."
+$javaCommand = "java -jar gestorPassword.jar"
+$runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
+Invoke-Expression $runCommand 
--- java-exe.ps1
+++
@@ -1,7 +0,0 @@
-$srcClases = ".\src\*.java .\src\Models\User\*.java .\src\Interface\Utils\*.java .\src\Interface\Panels\*.java .\src\Models\Cuenta\*.java "
-$libFiles = ".\lib\javaORM\javaORM.jar;"
-$compile = "javac -Werror -Xlint:all -d .\bin\ -cp '$libFiles' $srcClases"
-$createJar = "jar -cfm gestorPassword.jar Manifesto.txt -C .\bin\ ."
-$javaCommand = "java -jar gestorPassword.jar"
-$runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
-Invoke-Expression $runCommand 
