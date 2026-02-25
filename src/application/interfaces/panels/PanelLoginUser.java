62ce1cb6abc231f82986928a44b8842721dd9261
AlfonsoG-dev <alfgomachi@gmail.com>
2026-02-25 18:50:40 -0500

    refactor: decouple code, and create the variables assigning them later

--- src/application/interfaces/panels/PanelLoginUser.java
+++ src/application/interfaces/panels/PanelLoginUser.java
@@ -2,18 +2,19 @@ package application.interfaces.panels;
 
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 
 import application.interfaces.utils.PanelUtils;
 import application.models.user.UserModel;
@@ -27,7 +28,6 @@ import orm.utils.formats.ParamValue;
 public class PanelLoginUser {
 
     private JFrame myFrame;
-    private JPanel pPrincipal;
     private JTextField txtNombre;
     private JTextField txtEmail;
     private JTextField txtPassword;
@@ -47,75 +47,72 @@ public class PanelLoginUser {
         pOptions.setLayout(new GridLayout(4, 2));
 
         pOptions.add(new JLabel(" Nombre"));
-        pOptions.add(txtNombre = new JTextField());
+        txtNombre = new JTextField();
+        pOptions.add(txtNombre);
 
         pOptions.add(new JLabel(" Email"));
-        pOptions.add(txtEmail = new JTextField());
+        txtEmail = new JTextField();
+        pOptions.add(txtEmail);
 
         pOptions.add(new JLabel(" Password"));
-        pOptions.add(txtPassword = new JTextField());
+        txtPassword = new JTextField();
+        pOptions.add(txtPassword);
 
         pOptions.add(new JLabel(" Rol"));
-        pOptions.add(txtRol = new JTextField());
+        txtRol = new JTextField();
+        pOptions.add(txtRol);
 
         return pOptions;
     }
+    private void appendOKOptionAction(String[] column, String[] value, UserODM user) throws SQLException {
+        ParamValue condition = new ParamValue(column, value, "and");
+        userUtils.insertOperation(user, condition);
+        myFrame.dispose();
+        new PanelLogin(myConfig, cursor);
+    }
+    private String[] getTextFieldValues() {
+        String nombre   = !txtNombre.getText().isBlank() ? txtNombre.getText() : null;
+        String email    = !txtEmail.getText().isBlank() ? txtEmail.getText() : null;
+        String password = !txtPassword.getText().isBlank() ? txtPassword.getText() : null;
+        String rol      = !txtRol.getText().isBlank() ? txtRol.getText() : null;
+        return new String[] {nombre, email, password, rol};
+    }
     private void okButtonHandler(JButton okButton) {
         okButton.setMnemonic(KeyEvent.VK_ENTER);
-        okButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    String 
-                        nombre   = txtNombre.getText().isEmpty() == false ? txtNombre.getText() : "",
-                        email    = txtEmail.getText().isEmpty() == false ? txtEmail.getText() : "",
-                        password = txtPassword.getText().isEmpty() == false ? txtPassword.getText() : "",
-                        rol      = txtRol.getText().isEmpty() == false ? txtRol.getText() : "";
-                    if(nombre == "" || email == "" || password == "" || rol == "") {
-                        JOptionPane.showMessageDialog(
-                                myFrame,
-                                "Invalid data",
-                                "Error",
-                                JOptionPane.ERROR_MESSAGE
-                        );
-                    } else {
-                        UserODM newUser = new UserODM(nombre, email, password, rol);
-                        newUser.makeCreate_at();
-                        String[]
-                            c = {"nombre", "email"},
-                            v = {nombre, email};
-                        int options = JOptionPane.showConfirmDialog(
-                                myFrame,
-                                "Do you want to register?",
-                                "Register operation",
-                                JOptionPane.OK_CANCEL_OPTION,
-                                JOptionPane.QUESTION_MESSAGE
-                        );
-                        if(options == JOptionPane.OK_OPTION) {
-                            ParamValue condition = new ParamValue(c, v, "and");
-                            userUtils.insertOperation(newUser, condition);
-                            myFrame.dispose();
-                            new PanelLogin(myConfig, cursor);
-                        }
+        okButton.addActionListener(e -> {
+            try {
+                String nombre = getTextFieldValues()[0];
+                String email = getTextFieldValues()[1];
+                String password = getTextFieldValues()[2];
+                String rol = getTextFieldValues()[3];
+
+                if(nombre == null || email == null || password == null || rol == null) {
+                    JOptionPane.showMessageDialog(myFrame,
+                            "Invalid data", "Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    UserODM newUser = new UserODM(nombre, email, password, rol);
+                    newUser.makeCreate_at();
+                    String[] c = {"nombre", "email"};
+                    String[] v = {nombre, email};
+                    int options = JOptionPane.showConfirmDialog(myFrame,
+                            "Do you want to register?", "Register operation",
+                            JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+                    if(options == JOptionPane.OK_OPTION) {
+                        appendOKOptionAction(c, v, newUser);
                     }
-                } catch(Exception er) {
-                    er.printStackTrace();
                 }
+            } catch(Exception er) {
+                er.printStackTrace();
             }
         });
     }
     private void cancelButtonHandler(JButton cancelButton) {
-        cancelButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int options = JOptionPane.showConfirmDialog(
-                        myFrame,
-                        "Do you want to cancel?",
-                        "Register operation",
-                        JOptionPane.OK_CANCEL_OPTION,
-                        JOptionPane.QUESTION_MESSAGE
-                );
-                if(options == JOptionPane.OK_OPTION) {
-                    System.exit(0);
-                }
+        cancelButton.addActionListener(e -> {
+            int options = JOptionPane.showConfirmDialog(myFrame,
+                    "Do you want to cancel?", "Register operation",
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if(options == JOptionPane.OK_OPTION) {
+                System.exit(0);
             }
         });
     }
@@ -125,9 +122,9 @@ public class PanelLoginUser {
         JPanel options = new JPanel();
         options.setLayout(new FlowLayout());
 
-        JButton OKButton = new JButton("OK");
-        options.add(OKButton);
-        okButtonHandler(OKButton);
+        JButton btnOK = new JButton("OK");
+        options.add(btnOK);
+        okButtonHandler(btnOK);
 
         JButton cancelButton = new JButton("cancel");
         options.add(cancelButton);
@@ -141,14 +138,15 @@ public class PanelLoginUser {
         myFrame.setLayout(new GridLayout(3, 1));
         myFrame.addWindowListener(new WindowAdapter() {
             // changes the close operation
+            @Override
             public void windowClosing(WindowEvent we) {
                 System.exit(0);
             }
         });
 
-        JLabel header = new JLabel("Register user", JLabel.CENTER);
+        JLabel header = new JLabel("Register user", SwingConstants.CENTER);
 
-        pPrincipal = new JPanel();
+        JPanel pPrincipal = new JPanel();
         pPrincipal.setLayout(new GridLayout(2, 1));
 
         pPrincipal.add(optionsComponent());
@@ -158,6 +156,6 @@ public class PanelLoginUser {
         myFrame.add(pPrincipal);
 
         myFrame.setVisible(true);
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
